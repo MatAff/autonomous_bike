8725d84c60ea5759b06faf92922aecfdad0ecf07
MatAff <mathijsaffourtit@gmail.com>
2019-08-28 05:02:34 +0100

    changes

--- py/bike.py
+++ py/bike.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from adafruit_motorkit import MotorKit
+import time
 
 kit = MotorKit()
 
-for i in range(100):
-    kit.stepper2.onestep()
+t0 = time.time()
+for i in range(155):
+    kit.stepper1.onestep(direction=1, style=2)
+time.sleep(1)
+for i in range(155):
+    kit.stepper1.onestep(direction=0, style=2)
+t1 = time.time()
+print(t1 - t0)
+kit.stepper1.release()
