43f5cd001044dd211691ffd76c946260801dd8d9
MatAff <v.keller.86@gmail.com>
2019-11-13 03:41:50 +0000

    started MusicalMotor

--- py/stepper.py
+++ py/stepper.py
@@ -57,6 +57,32 @@ class BigBoy(object):
 #    def release(self):
 #        self.kit.stepper1.release()
 
+class MusicalMotor():
+    def __init__(self, mtr):
+        self.mtr = mtr
+        self.basenote = "e"
+        self.basefrequency = 1 / 0.0002
+        self.baseoctave = 5
+        self.noteletters = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"]
+        self.notes = {}
+        for octave in range(3,7):
+            for note in range(12):
+                power = 12 * (octave - self.baseoctave) + note - self.noteletters.index(self.basenote)
+                self.notes[str(octave) + self.noteletters[note]] = self.basefrequency * ((2 ** (1/12)) ** power)
+    
+    def playnote(self, note, notelength, pause):
+        print('playing note', note, self.notes[note])
+        self.mtr.step(notelength, self.notes[note])
+        print('pausing for', pause)
+        time.sleep(pause)
+
+    def printnotes(self):
+        for k in sorted(self.notes.keys()):
+            v = self.notes[k]
+            print(k, "\t", round(v, 8))
+
+
+
 if __name__ == "__main__":
 
     import sys
@@ -70,6 +96,16 @@ if __name__ == "__main__":
         pass
 
     mtr = BigBoy()
-    print('stepping', steps, 'steps over', wait, 's')
-    mtr.step(steps, wait)
+    #print('stepping', steps, 'steps over', wait, 's')
+    #start_time = time.time()
+    #mtr.step(steps, wait)
+    #print('Elapsed time: ', time.time() - start_time)
+
+    mm = MusicalMotor(mtr)
+    mm.printnotes()
+    notelength = 400
+    song = ["5c", "5c", "5d", "5c", "5f", "5e"]
+    for note in song:
+        mm.playnote(note, notelength, 0.1)
+
     mtr.cleanup()
